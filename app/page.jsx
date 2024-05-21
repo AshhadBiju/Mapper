448c5af6f4ef6242c36c9e63da4c224e46f36303
AshhadBiju <ashhadbiju2000@gmail.com>
2024-05-21 06:21:26 +0530

    login-prerequisites

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import { React, useState } from "react";
 import { useRouter } from "next/navigation";
 import {
   Tabs,
@@ -15,17 +15,7 @@ import Image from "next/image";
 import mapBg from "@/public/images/map-bg.svg";
 
 export default function Login() {
-  const [selected, setSelected] = React.useState("login");
-
-  // const router = useRouter();
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-
-  // const handleLogin = async (e) => {
-  //   e.preventDefault();
-  //   console.log("Logging in with:", { email, password });
-  //   router.push("/dashboard");
-  // };
+  const [selected, setSelected] = useState("login");
 
   return (
     <main className=" max-h-screen">
@@ -37,63 +27,12 @@ export default function Login() {
               src={mapBg}
               alt="map goes here"
               width={350}
-              // height={451}
               sizes="(min-width: 500px) 254px, (min-width: 780px) 147px, 254px"
-              // className="hidden md:block"
               priority={true}
             />
           </div>
         </div>
         <div className="w-full md:w-[50%] flex justify-center items-center">
-          {/* <div className="bg-slate-400 py-10 px-5 rounded w-full">
-            <div>
-              <h1 class="text-white text-xl md:text-3xl font-bold mb-5">
-                Login
-              </h1>
-            </div>
-
-            <form onSubmit={handleLogin} class="space-y-4">
-              <div>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  class="w-full px-4 py-3 rounded-lg bg-white text-black focus:outline-blue-500"
-                  required
-                />
-              </div>
-              <div>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  class="w-full px-4 py-3 rounded-lg bg-white text-black focus:outline-blue-500"
-                  required
-                />
-              </div>
-              <Link href="/forgotPassword">
-                <h1 class="text-sky-500 text-base md:text-xl font-semibold hover:text-blue-600 my-4">
-                  Forgot Password ?
-                </h1>
-              </Link>
-              <button
-                type="submit"
-                class="w-full bg-blue-500 text-white py-3 rounded-lg font-semibold transition duration-300 hover:bg-blue-600 "
-              >
-                Map your way in
-              </button>
-            </form>
-            <div className="text-white flex gap-1 justify-center text-base md:text-xl mt-4">
-              <h1>Don't have an account ?</h1>
-              <Link href="/signup">
-                <h1 class="text-sky-500  font-semibold hover:text-blue-600">
-                  Sign up
-                </h1>
-              </Link>
-            </div>
-          </div> */}
           <div className="max-w-full w-[1000px] h-[420px] max-h-full mb-8">
             <Card>
               <CardBody className="overflow-hidden">
@@ -113,14 +52,12 @@ export default function Login() {
                     <form className="flex flex-col gap-4">
                       <Input
                         isRequired
-                        // label="Email"
                         placeholder="Enter your email"
                         type="email"
                         size="lg"
                       />
                       <Input
                         isRequired
-                        // label="Password"
                         placeholder="Enter your password"
                         type="password"
                         size="lg"
@@ -155,28 +92,24 @@ export default function Login() {
                     <form className="flex flex-col gap-4">
                       <Input
                         isRequired
-                        // label="Name"
                         placeholder="Enter your name"
                         type="password"
                         size="lg"
                       />
                       <Input
                         isRequired
-                        // label="Email"
                         placeholder="Enter your email"
                         type="email"
                         size="lg"
                       />
                       <Input
                         isRequired
-                        // label="Password"
                         placeholder="Enter Password"
                         type="password"
                         size="lg"
                       />
                       <Input
                         isRequired
-                        // label="Confirm Password"
                         placeholder="Confirm password"
                         type="password"
                         size="lg"
