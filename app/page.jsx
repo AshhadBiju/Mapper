54aca0a0321a592978f0968a3d21efa2c76190bb
AshhadBiju <ashhadbiju2000@gmail.com>
2024-05-21 08:23:00 +0530

    remove-login-Home

--- app/page.jsx
+++ app/page.jsx
@@ -17,16 +17,6 @@ import Login from "@/app/components/login";
 import Signup from "@/app/components/sign-up";
 export default function Home() {
   const [selected, setSelected] = useState("login");
-  // LOGIN
-  // const [loginEmail, setLoginEmail] = useState("");
-  // const [loginPassword, setLoginPassword] = useState("");
-
-  // SIGN UP
-  // const [signupName, setSignupName] = useState("");
-  // const [signupEmail, setSignupEmail] = useState("");
-  // const [signupPassword, setSignupPassword] = useState("");
-  // const [signupConfirmPassword, setSignupConfirmPassword] = useState("");
-
   return (
     <main className=" max-h-screen">
       <section className="flex flex-col md:flex-row md:gap-x-4 gap-y-4 h-screen w-full md:p-4 px-4 pt-0 pb-4">
@@ -60,44 +50,6 @@ export default function Home() {
                     className="text-2xl md:text-3xl m-2"
                   >
                     <Login />
-                    {/* <form className="flex flex-col gap-4">
-                      <Input
-                        isRequired
-                        placeholder="Enter your email"
-                        type="email"
-                        size="lg"
-                        value={loginEmail}
-                        onChange={(e) => setLoginEmail(e.target.value)}
-                      />
-                      <Input
-                        isRequired
-                        placeholder="Enter your password"
-                        type="password"
-                        size="lg"
-                        value={loginPassword}
-                        onChange={(e) => setLoginPassword(e.target.value)}
-                      />
-                      <p className="text-center text-xl">
-                        Need to create an account ?
-                        <Link
-                          size="lg"
-                          onPress={() => setSelected("sign-up")}
-                          className="ml-2 cursor-pointer hover:text-blue-600"
-                        >
-                          Sign up
-                        </Link>
-                      </p>
-                      <div className="flex gap-2 justify-end">
-                        <Button
-                          fullWidth
-                          color="primary"
-                          className="text-2xl"
-                          size="md"
-                        >
-                          Map your way in
-                        </Button>
-                      </div>
-                    </form> */}
                   </Tab>
                   <Tab
                     key="sign-up"
@@ -105,62 +57,6 @@ export default function Home() {
                     className="text-2xl md:text-3xl m-2"
                   >
                     <Signup />
-                    {/* <form className="flex flex-col gap-4">
-                      <Input
-                        isRequired
-                        placeholder="Enter your name"
-                        type="password"
-                        size="lg"
-                        value={signupName}
-                        onChange={(e) => setSignupName(e.target.value)}
-                      />
-                      <Input
-                        isRequired
-                        placeholder="Enter your email"
-                        type="email"
-                        size="lg"
-                        value={signupEmail}
-                        onChange={(e) => setSignupEmail(e.target.value)}
-                      />
-                      <Input
-                        isRequired
-                        placeholder="Enter Password"
-                        type="password"
-                        size="lg"
-                        value={signupPassword}
-                        onChange={(e) => setSignupPassword(e.target.value)}
-                      />
-                      <Input
-                        isRequired
-                        placeholder="Confirm password"
-                        type="password"
-                        size="lg"
-                        value={signupConfirmPassword}
-                        onChange={(e) =>
-                          setSignupConfirmPassword(e.target.value)
-                        }
-                      />
-                      <p className="text-center text-xl">
-                        Already have an account ?
-                        <Link
-                          size="lg"
-                          onPress={() => setSelected("login")}
-                          className="ml-2 cursor-pointer hover:text-blue-900"
-                        >
-                          Login
-                        </Link>
-                      </p>
-                      <div className="flex gap-2 justify-end">
-                        <Button
-                          fullWidth
-                          color="primary"
-                          className="text-2xl "
-                          size="md"
-                        >
-                          Sign up to enter Mapper
-                        </Button>
-                      </div>
-                    </form> */}
                   </Tab>
                 </Tabs>
               </CardBody>
